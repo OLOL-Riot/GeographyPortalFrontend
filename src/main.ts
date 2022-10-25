ab4b19274ce29d7632969afd9f3520e067652d60
“KruglovIlya” <kruglov-il1@yandex.ru>
2022-10-26 01:05:51 +0400

    add quasar lang pack

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { Quasar } from "quasar";
+import quasarLang from 'quasar/lang/ru'
 
 // Import icon libraries
 import "@quasar/extras/material-icons/material-icons.css";
