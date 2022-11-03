1661f8063b5bcedac27fbc59e8dd9fdc6760ef84
ElizavetaGll <ellizavetagll@gmail.com>
2022-11-03 17:31:07 +0400

    add bottons

--- src/components/ExerciseList.vue
+++ src/components/ExerciseList.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
 import { getApi } from "@/api";
+import { ref } from "vue";
 
 const api = getApi();
 
-api.get("api/Exercise/solve").then((response) => console.log);
+interface IExercise {
+  ansvers: Array<string>;
+  description: string;
+  id: string;
+  serialNumber: number;
+}
+
+interface IExerciseList extends Array<IExercise> {}
+
+const exercises = ref(Array<IExercise>);
+
+api.get("api/Exercise/solve").then((response) => {
+  exercises.value = response.data;
+});
 </script>
 
 <template>
-  <div class="greetings"></div>
+  <div class="q-pa-md row items-start q-gutter-md q-mt-xl">
+    <q-btn
+      color="primary"
+      :label="item.description"
+      v-for="item in exercises"
+      :key="item.serialNumber"
+    />
+  </div>
 </template>
