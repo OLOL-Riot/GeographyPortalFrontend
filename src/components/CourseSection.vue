86a43c170e84f8f003c7f247c17906340f1d591a
Dmutaliev <dmutaliev@bug-busters.ru>
2022-11-24 23:39:36 +0400

    feat(CourseSection.vue): add button for back to the last page

--- src/components/CourseSection.vue
+++ src/components/CourseSection.vue
@@ -109,6 +109,11 @@ api
       <q-btn class="q-mx-auto" color="green" label="Перейти к тесту"
         @click="$router.push({ name: 'test', params: { testId: testId } })" />
     </div>
+
+    <div class="row q-pa-md">
+      <q-btn class="q-mx-auto" color="grey" label="Назад"
+        @click="$router.back()" />
+    </div>
   </div>
 </template>
 
