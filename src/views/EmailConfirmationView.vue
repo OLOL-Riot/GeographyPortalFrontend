32648c5e2bcb67470bf6ccc0907f09ffc0ac4c79
Dmutaliev <dmutaliev@bug-busters.ru>
2022-12-20 22:35:44 +0400

    feat

--- src/views/EmailConfirmationView.vue
+++ src/views/EmailConfirmationView.vue
@@ -13,26 +13,35 @@ const id = ref(route.query.id as string);
 const token = ref(route.query.token as string);
 const isConfirmed = ref(false as boolean);
 
-api.get("/api/Authentification/confirm-email?id=" + id.value + "&token=" + token.value)
-    .then((response) => {
-        isConfirmed.value = true;
-    })
-    .catch((err: AxiosError) => {
-        $q.notify({
-            color: "red-5",
-            textColor: "white",
-            icon: "warning",
-            message: err.message,
+function confirmEmail() {
+    const formattedToken = token.value.replace("+", "%2B").replace("/", "%2F").replace("==", "%3D%3D");
+
+    api.get("/api/Authentification/confirm-email?id=" + id.value + "&token=" + formattedToken)
+        .then((response) => {
+            isConfirmed.value = true;
+        })
+        .catch((err: AxiosError) => {
+            $q.notify({
+                color: "red-5",
+                textColor: "white",
+                icon: "warning",
+                message: err.message,
+            });
         });
-    });
+}
+
+
 
 </script>
 
 <template>
-    <div class="container ">
-        <div class="page-confirm-email-title">
+    <div class="container">
+        <div class="row q-pa-md">
+            <q-btn class="q-mx-auto" color="blue" label="Подтвердить почту" @click="confirmEmail()" />
+        </div>
+        <div class="page-confirm-email-result">
             <h1 class="text-h4 q-mx-auto text-center" v-if="isConfirmed">Почта успешно подтверждена!</h1>
-            <h1 class="text-h4 q-mx-auto text-center" v-else>Почта не подтверждена!</h1>
+            <h1 class="text-h4 q-mx-auto text-center" v-else>Почта ещё не подтверждена!</h1>
         </div>
     </div>
 </template>
