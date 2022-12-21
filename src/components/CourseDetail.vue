91846d3f50e1c69e14cd8106393eecb5ef48c1eb
KruglovIlya <kriglov-il1@yandex.ru>
2022-12-21 13:27:39 +0400

    refactoring

--- src/components/CourseDetail.vue
+++ src/components/CourseDetail.vue
@@ -1,38 +1,14 @@
 <script setup lang="ts">
-import { getApi } from "@/api";
-import type { AxiosError, AxiosResponse } from "axios";
-import type { ICourse } from "@/interfaces/ICourse";
-import { useQuasar, QBtn } from "quasar";
-import { ref } from "vue";
-import { useRoute } from "vue-router";
-
 const props = defineProps({
-  courseId: {
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
     type: String,
     required: true,
   },
 });
-
-const $q = useQuasar();
-const route = useRoute();
-
-const course = ref({} as ICourse);
-
-getApi().then((api) =>
-  api
-    .get("api/Course/page/" + props.courseId)
-    .then((response: AxiosResponse<ICourse>) => {
-      course.value = response.data;
-    })
-    .catch((err: AxiosError) => {
-      $q.notify({
-        color: "red-5",
-        textColor: "white",
-        icon: "warning",
-        message: err.message,
-      });
-    })
-);
 </script>
 
 <template>
@@ -40,41 +16,11 @@ getApi().then((api) =>
     <div class="row">
       <div class="col-12">
         <h1 class="text-h1">
-          {{ course.name }}
+          {{ name }}
         </h1>
-        <div class="text-body1 q-mt-md" v-html="course.description">
-        </div>
+        <div class="text-body1 q-mt-md" v-html="description"></div>
       </div>
     </div>
-    <div class="row">
-      <div class="col-12">
-        <h3 class="text-h3 q-px-sm q-py-md">Секции курса</h3>
-      </div>
-      <div
-        class="col-md-4 q-px-sm q-py-md"
-        v-for="section in course.previewCourseSections"
-        :key="section.id"
-      >
-        <q-card>
-          <q-card-section class="bg-grey-8 text-white">
-            <div class="text-h6">{{ section.name }}</div>
-            <div class="text-subtitle2">{{ section.shortDescription }}</div>
-          </q-card-section>
-
-          <q-card-actions vertical class="no-padding">
-            <q-btn
-              flat
-              @click="
-                $router.push({
-                  name: 'courseSection',
-                  params: { courseSectionId: section.id },
-                })
-              "
-              >Перейти</q-btn
-            >
-          </q-card-actions>
-        </q-card>
-      </div>
-    </div>
+    <slot></slot>
   </div>
 </template>
---
+++ src/components/CourseSectionPreview.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  shortDescription: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="col-md-4 q-px-sm q-py-md">
+    <q-card>
+      <q-card-section class="bg-grey-8 text-white">
+        <div class="text-h6">{{ name }}</div>
+        <div class="text-subtitle2">{{ shortDescription }}</div>
+      </q-card-section>
+
+      <q-card-actions vertical class="no-padding">
+        <q-btn
+          flat
+          @click="
+            $router.push({
+              name: 'courseSection',
+              params: { courseSectionId: id },
+            })
+          "
+          >Перейти</q-btn
+        >
+      </q-card-actions>
+    </q-card>
+  </div>
+</template>
